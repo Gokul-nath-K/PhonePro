390a178158d2caad46abfc6fc211a7a9a4d439f7
Gokul-nath-K <gokulnathk003@gmail.com>
2023-09-29 09:43:36 +0530

    Entry point created

---
+++ PhoneBookApp/Backend/src/main/java/com/phoneBookApp/services/EntryService.java
@@ -0,0 +1,69 @@
+package com.phoneBookApp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phoneBookApp.entity.User;
+import com.phoneBookApp.repository.UserRepository;
+
+@Service
+public class EntryService {
+
+	@Autowired
+	UserRepository uRepo;
+	
+	
+	
+	public Boolean validateUser(String email, String password) {
+		
+		List<User> userlist = uRepo.checkPassword(email);
+		String ref = "";
+		
+		if(userlist.isEmpty()) {
+			
+			return false;
+		}
+		else {
+						
+			ref = userlist.get(0).getPassword();
+		}
+		
+		
+		if(password.equals(ref)) {
+			
+			return true;
+		}
+		else if( !password.equals(ref) ) {
+			
+			 return false;
+		}
+		else {
+			
+			return false;
+		}
+		
+	}
+	
+	
+	public Integer CreateNewUser(User new_user) {
+		
+		List<Integer> emailCountList = uRepo.isEmailExist(new_user.getEmail());
+		List<Integer> phnoCountList = uRepo.isPhnoExist(new_user.getPhone_no());
+		
+		if(emailCountList.get(0) !=  0) {
+			
+			return 1;
+		}
+		else if(phnoCountList.get(0) != 0) {
+			
+			return 2;
+		}
+		else {
+			
+			uRepo.save(new_user);
+			return 0;
+		}
+	}
+}
