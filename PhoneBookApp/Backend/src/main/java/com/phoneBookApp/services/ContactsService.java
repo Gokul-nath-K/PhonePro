968aa74711c69e0162de5e3a994df5197f7ee97c
Gokul-nath-K <gokulnathk003@gmail.com>
2023-09-29 14:21:21 +0530

    Update ContactsService.java

--- PhoneBookApp/Backend/src/main/java/com/phoneBookApp/services/ContactsService.java
+++ PhoneBookApp/Backend/src/main/java/com/phoneBookApp/services/ContactsService.java
@@ -1,13 +1,28 @@
 package com.phoneBookApp.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.phoneBookApp.entity.Contacts;
+import com.phoneBookApp.entity.UserContacts;
 import com.phoneBookApp.repository.ContactsRepository;
+import com.phoneBookApp.repository.UserContactsRepository;
 
 @Service
 public class ContactsService {
 
 	@Autowired
 	ContactsRepository cRepo;
+	
+	@Autowired
+	UserContactsRepository uRepo;
+	
+	public List<Contacts> getAllContacts(Long user_id) {
+		
+		UserContacts user = uRepo.findById(user_id).get();
+		
+		return user.getContacts();
+	}
 }
