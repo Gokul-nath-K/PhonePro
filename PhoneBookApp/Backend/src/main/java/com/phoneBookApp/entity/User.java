36e8ac76bc9ae3dfcd115d7581350b5fc98beceb
Gokul-nath-K <gokulnathk003@gmail.com>
2023-09-29 14:20:46 +0530

    Update User.java

--- PhoneBookApp/Backend/src/main/java/com/phoneBookApp/entity/User.java
+++ PhoneBookApp/Backend/src/main/java/com/phoneBookApp/entity/User.java
@@ -1,12 +1,9 @@
 package com.phoneBookApp.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
