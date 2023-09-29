495393c68cfe36a9c8d82ef9b38f376f2353406b
Gokul-nath-K <gokulnathk003@gmail.com>
2023-09-29 14:20:27 +0530

    Update ContactsController.java

--- PhoneBookApp/Backend/src/main/java/com/phoneBookApp/controller/ContactsController.java
+++ PhoneBookApp/Backend/src/main/java/com/phoneBookApp/controller/ContactsController.java
@@ -2,30 +2,40 @@ package com.phoneBookApp.controller;
 
 import java.util.List;  
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.phoneBookApp.entity.Contacts;
-import com.phoneBookApp.entity.UserMid;
+import com.phoneBookApp.entity.UserContacts;
 import com.phoneBookApp.repository.ContactsRepository;
-import com.phoneBookApp.repository.UserMidRepository;
+import com.phoneBookApp.repository.UserContactsRepository;
+import com.phoneBookApp.services.ContactsService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 public class ContactsController {
 
 	@Autowired
-	ContactsRepository cRepo;
+	ContactsService cService;
 	
-	@Autowired
-	UserMidRepository uRepo;
-	
+	@Operation(summary = "Retrive all contacts of particular user")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Contacts retrived successfully"),
+			@ApiResponse(responseCode = "400", description = "Not found")
+	})
+	@ResponseStatus(HttpStatus.FOUND)
 	@GetMapping("/{user_id}/getAll")
-	public List<Contacts> getAllContacts(@PathVariable(value = "user_id") Long user_id) {
+	public ResponseEntity<List<Contacts>> getAllContacts(@PathVariable(value = "user_id") Long user_id) {
 		
-		UserMid user = uRepo.findById(user_id).get();
-		
-		return user.getContacts();
+		return ResponseEntity.ok(cService.getAllContacts(user_id));
 	}
 }
