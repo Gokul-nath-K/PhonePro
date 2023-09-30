a27d52e47a0d4c29ccaf81e3f85bd5246cd29bb5
Gokul-nath-K <gokulnath@techmolders.com>
2023-09-30 20:57:48 +0530

    Update SearchPage.jsx

--- PhoneBookApp/Frontend/src/Pages/SearchPage.jsx
+++ PhoneBookApp/Frontend/src/Pages/SearchPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import SearchBar from '../Components/SearchBar'
-import { contacts } from '../Assets/Contexts/Contacts';
+import React, { useState } from "react";
+import SearchBar from "../Components/SearchBar";
+import { contacts } from "../Assets/Contexts/Contacts";
 
 function SearchPage() {
   const [searchInput, setSearchInput] = useState("");
@@ -18,35 +18,42 @@ function SearchPage() {
       return el.name.toLowerCase().includes(searchInput);
     }
   });
-  const result = "Results...";
 
   return (
     <>
-        <div className="container-fluid pt-5" style={{ maxWidth : "80%", paddingRight : "5%"}}>
-            <div className="row">
-                <SearchBar handleChange={handleChange}/>
-            </div>
-            <div className="row pt-4 h-100">
-              {
-                filteredData && filteredData.map((contact) => {
-                  return (
-                    <div key={contact.id}>
-                        <div className="row bg-transparent border-bottom py-2">
-                          <div className="col-1">
-                            <button type="button" class="btn btn-secondary rounded-circle" disabled>{ contact.name.slice(0,1) }</button>
-                          </div>
-                          <div className="col">
-                              <p className='pt-2 text-light'> {  contact.name } </p>
-                          </div>
-                        </div>
-                      </div>
-                  )
-                })
-              }
-            </div>
+      <div
+        className="container-fluid pt-5"
+        style={{ maxWidth: "80%", paddingRight: "5%" }}
+      >
+        <div className="row">
+          <SearchBar handleChange={handleChange} />
         </div>
+        <div className="row pt-4 h-100">
+          {filteredData &&
+            filteredData.map((contact) => {
+              return (
+                <div key={contact.id}>
+                  <div className="row bg-transparent border-bottom py-2">
+                    <div className="col-1">
+                      <button
+                        type="button"
+                        className="btn btn-secondary rounded-circle"
+                        disabled
+                      >
+                        {contact.name.slice(0, 1)}
+                      </button>
+                    </div>
+                    <div className="col">
+                      <p className="pt-2 text-light"> {contact.name} </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default SearchPage
+export default SearchPage;
