ebf35b6a67930c1d7efd54d92760c13c57c976e6
Gokul-nath-K <gokulnathk003@gmail.com>
2023-09-30 23:30:08 +0530

    Delete contacts added

--- PhoneBookApp/Backend/src/main/java/com/phoneBookApp/controller/UserContactsController.java
+++ PhoneBookApp/Backend/src/main/java/com/phoneBookApp/controller/UserContactsController.java
@@ -1,14 +1,22 @@
 package com.phoneBookApp.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.phoneBookApp.entity.Contacts;
+import com.phoneBookApp.entity.User;
 import com.phoneBookApp.entity.UserContacts;
 import com.phoneBookApp.services.UserContactsService;
 
@@ -17,6 +25,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user_contact")
 public class UserContactsController {
 
@@ -34,6 +43,19 @@ public class UserContactsController {
 	public ResponseEntity<UserContacts> postUser(@RequestBody UserContacts u) {
 		
 		return ResponseEntity.ok(service.postUser(u));
+		
+	}
+	
+	@Operation(summary = "Delete contact of particular user")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Contact deleted  successfully"),
+			@ApiResponse(responseCode = "400", description = "Could not delete contact")
+	})
+	@ResponseStatus(HttpStatus.OK)
+	@DeleteMapping("/delete_contact")
+	public ResponseEntity<UserContacts> postUser(@RequestBody Pair<User, Contacts> pair) {
+		
+		return ResponseEntity.ok(service.deleteContact(pair.getFirst(), pair.getSecond()));
 		
 	}
 }
--- PhoneBookApp/Backend/src/main/java/com/phoneBookApp/services/UserContactsService.java
+++ PhoneBookApp/Backend/src/main/java/com/phoneBookApp/services/UserContactsService.java
@@ -1,11 +1,16 @@
 package com.phoneBookApp.services;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.phoneBookApp.entity.Contacts;
+import com.phoneBookApp.entity.User;
 import com.phoneBookApp.entity.UserContacts;
+import com.phoneBookApp.repository.ContactsRepository;
 import com.phoneBookApp.repository.UserContactsRepository;
 
 @Service
@@ -14,21 +19,49 @@ public class UserContactsService {
 	@Autowired
 	UserContactsRepository umRepo;
 	
+	@Autowired
+	ContactsRepository cRepo;
+	
 	public UserContacts postUser(UserContacts u) {
+			
+			List<UserContacts> user = umRepo.getByPhno(u.getUser().getPhone_no());
+			
+			if(user.size() != 0) {
+				
+				UserContacts oldUser = umRepo.findById(user.get(0).getId()).get();
+				
+				oldUser.getContacts().add(u.getContacts().get(0));
+				
+				return umRepo.save(oldUser);
+			}
+			else {
+				
+				return umRepo.save(u);
+			}
+	}
+	
+	
+	public UserContacts deleteContact(User u, Contacts contact) {
 		
-		List<UserContacts> user = umRepo.getByPhno(u.getUser().getPhone_no());
+		List<UserContacts> user = umRepo.getByPhno(u.getPhone_no());
 		
 		if(user.size() != 0) {
 			
-			UserContacts oldUser = umRepo.findById(user.get(0).getId()).get();
+			UserContacts oldUser = user.get(0);
 			
-			oldUser.getContacts().add(u.getContacts().get(0));
+			List<Contacts> contacts = oldUser.getContacts();
+			
+			contacts.removeIf(c -> c.getPhone_no().longValue() == contact.getPhone_no().longValue());
+			
+			oldUser.setContacts(contacts);
+			
+			cRepo.deleteById(contact.getId());
 			
 			return umRepo.save(oldUser);
 		}
 		else {
 			
-			return umRepo.save(u);
+			return umRepo.save(user.get(0));
 		}
 	}
 }
