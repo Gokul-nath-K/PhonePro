c992da524a26924a179961e70fc35501909dd349
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-02 20:22:26 +0530

    Backed folder moved

--- PhoneBookApp/Backend/src/main/java/com/phoneBookApp/repository/ContactsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.phoneBookApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.phoneBookApp.entity.Contacts;
-
-public interface ContactsRepository extends JpaRepository<Contacts, Long> {
-
-}
