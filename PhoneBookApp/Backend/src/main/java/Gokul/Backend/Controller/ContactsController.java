9676b9abccd60eeb842951e2acf66c1d3c1e3d80
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-04 09:28:42 +0530

    Create ContactsController.java

---
+++ PhoneBookApp/Backend/src/main/java/Gokul/Backend/Controller/ContactsController.java
@@ -0,0 +1,121 @@
+package Gokul.Backend.Controller;
+
+import java.util.List;  
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import Gokul.Backend.Entity.Contacts;
+import Gokul.Backend.Service.ContactsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactsController {
+
+	@Autowired
+	ContactsService cService;
+	
+	@Operation(summary = "Get all contacts")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "Contacts list retrieved successfully")
+	})
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Contacts>> getAll() {
+
+	    List<Contacts> allContacts = cService.getAll();
+	    return ResponseEntity.ok(allContacts);
+	}
+	
+	
+	@Operation(summary = "Get contact by user ID")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "Contact retrieved successfully"),
+	        @ApiResponse(responseCode = "404", description = "Contact not found")
+	})
+	@GetMapping("/getContact/{user_id}")
+	public ResponseEntity<List<Contacts>> getContactByUserId(@PathVariable("user_id") long user_id) {
+
+	    List<Contacts> contact = cService.getContactByUserId(user_id);
+	  
+	    return ResponseEntity.ok(contact);
+	}
+	
+	
+	@Operation(summary = "Get sorted contacts by name")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "Sorted contacts list retrieved successfully")
+	})
+	@GetMapping("/getSortedContacts/{dir}")
+	public ResponseEntity<List<Contacts>> getContactsSortedByName(@PathVariable("dir") String dir) {
+
+	    List<Contacts> sortedContacts = cService.getContactsSortedByName(dir);
+	    return ResponseEntity.ok(sortedContacts);
+	}
+	
+	
+	@Operation(summary = "Add a new contact")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "201", description = "Contact detail added successfully"),
+	        @ApiResponse(responseCode = "400", description = "Invalid contact detail")
+	})
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(value = "/post", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<Contacts> postContact(@RequestBody Contacts contact) {
+
+	    Contacts createdContact = cService.postContact(contact);
+	    return ResponseEntity.ok(createdContact);
+	}
+	
+	
+	@Operation(summary = "Update an existing contact")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "Contact updated successfully"),
+	        @ApiResponse(responseCode = "404", description = "Contact not found")
+	})
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Contacts> updateContact(@RequestBody Contacts contact, @PathVariable("id") Long id) {
+
+	    Contacts updatedContact = cService.updateContact(contact, id);
+	    
+	    return ResponseEntity.ok(updatedContact);
+	}
+	
+	
+	@Operation(summary = "Delete a contact by ID")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "Contact deleted successfully"),
+	        @ApiResponse(responseCode = "404", description = "Contact not found")
+	})
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Void> deleteContact(@PathVariable("id") Long id) {
+
+	    cService.deleteContact(id);
+	    return ResponseEntity.ok().build();
+	}
+	
+	
+	@Operation(summary = "Delete all contacts")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "All contacts deleted successfully")
+	})
+	@DeleteMapping("/deleteAll")
+	public ResponseEntity<Void> deleteAllContacts() {
+
+	    cService.deleteAllContacts();
+	    return ResponseEntity.ok().build();
+	}
+}
