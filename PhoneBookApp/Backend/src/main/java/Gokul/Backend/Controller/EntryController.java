1549717c643b80b358359f86246245881fa533ef
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-05 09:35:24 +0530

    Update EntryController.java

--- PhoneBookApp/Backend/src/main/java/Gokul/Backend/Controller/EntryController.java
+++ PhoneBookApp/Backend/src/main/java/Gokul/Backend/Controller/EntryController.java
@@ -3,23 +3,25 @@ package Gokul.Backend.Controller;
 import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import Gokul.Backend.Entity.User;
+import Gokul.Backend.DTO.AuthenticationRequest;
+import Gokul.Backend.DTO.AuthenticationResponse;
+import Gokul.Backend.DTO.RegisterRequest;
 import Gokul.Backend.Service.EntryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 
 @RestController
-@RequestMapping("/entry")
+@RequestMapping("/api/v1/entry")
+@RequiredArgsConstructor
 public class EntryController {
 
 	@Autowired
@@ -33,9 +35,9 @@ public class EntryController {
 	})
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping(value = "/post",  consumes = "application/json")
-	public ResponseEntity<Integer> CreateNewUser(@RequestBody User new_user) {
+	public ResponseEntity<AuthenticationResponse> CreateNewUser(@RequestBody RegisterRequest request) {
 		
-		return ResponseEntity.ok(eService.CreateNewUser(new_user));
+		return ResponseEntity.ok(eService.CreateNewUser(request));
 	}
 
 	
@@ -47,10 +49,10 @@ public class EntryController {
 			@ApiResponse(responseCode = "400", description = "Could not log in")
 	})
 	@ResponseStatus(HttpStatus.OK)
-	@GetMapping(value = "/auth_user")
-	public ResponseEntity<Boolean> checkUser(@RequestParam(value="email", required = true) String email, @RequestParam(value="password", required = true) String password) {
+	@PostMapping(value = "/auth_user")
+	public ResponseEntity<AuthenticationResponse> validateUser(@RequestBody AuthenticationRequest request) {
 
-		return ResponseEntity.ok(eService.validateUser(email, password));
+		return ResponseEntity.ok(eService.validateUser(request));
 	}
 	
 	
