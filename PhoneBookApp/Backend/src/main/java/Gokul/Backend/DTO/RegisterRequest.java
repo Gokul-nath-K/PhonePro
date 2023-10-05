fb15487af70df331a3db1b764f7a77331206b453
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-05 09:35:56 +0530

    Create RegisterRequest.java

---
+++ PhoneBookApp/Backend/src/main/java/Gokul/Backend/DTO/RegisterRequest.java
@@ -0,0 +1,20 @@
+package Gokul.Backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+	
+	private String name;
+	private Long phoneno;
+	private String dob;
+	private String email;
+	private String password;
+}
