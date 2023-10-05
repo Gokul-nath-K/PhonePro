c22c6e1edf9668ff42d30cec9b7a5863209cfaa3
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-05 09:36:11 +0530

    Update UserRepository.java

--- PhoneBookApp/Backend/src/main/java/Gokul/Backend/Repository/UserRepository.java
+++ PhoneBookApp/Backend/src/main/java/Gokul/Backend/Repository/UserRepository.java
@@ -1,6 +1,7 @@
 package Gokul.Backend.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,7 @@ import jakarta.transaction.Transactional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	Optional<User> findByEmail(String email);
 	
 	@Query("select u from User u where u.email = ?1")
 	public List<User> checkPassword(@Param("email") String email);
