8d004e49b690eaeee3283f0fd9dc5cacd4b2eaab
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-10 02:47:40 +0530

    Update LandingPage.jsx

--- PhoneBookApp/Frontend/src/Pages/LandingPage.jsx
+++ PhoneBookApp/Frontend/src/Pages/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-bootstrap-icons";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
 import LandingContent from "../Components/LandingContent";
