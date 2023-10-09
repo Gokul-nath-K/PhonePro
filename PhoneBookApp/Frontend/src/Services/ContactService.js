9eeaccc75a80e98fb0507646f45f448621916c22
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-10 02:47:54 +0530

    Update ContactService.js

--- PhoneBookApp/Frontend/src/Services/ContactService.js
+++ PhoneBookApp/Frontend/src/Services/ContactService.js
@@ -2,37 +2,37 @@ import axios from "axios";
 import { BASE_URL, HEADERS } from "../Constants/ApiConstant";
 
 const getAllContacts = async () => {
-    const response = await axios.get(`${BASE_URL}/api/contacts/getAll`, { HEADERS });
+    const response = await axios.get(`${BASE_URL}/contact/getAll`, { headers : HEADERS });
     return response.data;
 };
 
-const getContactByUserId = async (userId, headers) => {
-    const response = await axios.get(`${BASE_URL}/api/contacts/getContact/${userId}`, { HEADERS });
+const getContactByUserId = async (userId) => {
+    const response = await axios.get(`${BASE_URL}/contact/getContact/${userId}`, { headers : HEADERS });
     return response.data;
 };
 
 
 const getContactsSortedByName = async (dir) => {
-    const response = await axios.get(`${BASE_URL}/api/contacts/getSortedContacts/${dir}`, { HEADERS });
+    const response = await axios.get(`${BASE_URL}/contact/getSortedContacts/${dir}`, { headers : HEADERS });
     return response.data;
 };
 
 const postContact = async (contact) => {
-    const response = await axios.post(`${BASE_URL}/api/contacts/post`, contact, { HEADERS });
+    const response = await axios.post(`${BASE_URL}/contact/post`, contact, { headers : HEADERS });
     return response.data;
 };
 
 const updateContact = async (contact, id) => {
-    const response = await axios.put(`${BASE_URL}/api/contacts/update/${id}`, contact, { HEADERS });
+    const response = await axios.put(`${BASE_URL}/contact/update/${id}`, contact, { headers : HEADERS });
     return response.data;
 };
 
 const deleteContact = async (id) => {
-    await axios.delete(`${BASE_URL}/api/contacts/delete/${id}`, { HEADERS });
+    await axios.delete(`${BASE_URL}/contact/delete/${id}`, { headers : HEADERS });
 };
 
 const deleteAllContacts = async () => {
-    await axios.delete(`${BASE_URL}/api/contacts/deleteAll`, { HEADERS });
+    await axios.delete(`${BASE_URL}/contact/deleteAll`, { headers : HEADERS });
 };
 
 export const ContactService = {
