0034ea8800f549310c7d2c293342ab2fe4f9ece3
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-10 02:47:12 +0530

    Create axios.interceptors.js

---
+++ PhoneBookApp/Frontend/src/Config/axios.interceptors.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      // Token is expired
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(error);
+  }
+);
