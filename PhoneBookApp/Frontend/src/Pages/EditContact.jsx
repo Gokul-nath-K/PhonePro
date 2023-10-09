6e5a598ca685a4c926f363a4cd4600ab09c15c22
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-10 02:47:32 +0530

    Update EditContact.jsx

--- PhoneBookApp/Frontend/src/Pages/EditContact.jsx
+++ PhoneBookApp/Frontend/src/Pages/EditContact.jsx
@@ -1,71 +1,105 @@
-import React from 'react'
-import store from '../Assets/Redux/Store'
-
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { getEditedContacts } from "../Assets/Slices/ContactSlice";
+import { ContactService } from "../Services/ContactService";
+import { useNavigate } from "react-router-dom";
 function EditContact() {
+  const editedContact = useSelector(getEditedContacts);
+  const [contact, setContact] = useState({
+    id: editedContact.id,
+    name: editedContact.name,
+    phoneno: editedContact.phoneno,
+    email: editedContact.email,
+    groupname: editedContact.groupname,
+    user: {
+      id: editedContact.user.id,
+      name: editedContact.user.username,
+      email: editedContact.user.email,
+      phoneno: editedContact.user.phoneno,
+      dob: editedContact.user.dob,
+      password: editedContact.user.password,
+      role: editedContact.user.role,
+    },
+  });
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+  console.log(editedContact);
+  const handleChange = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+
+    try {
+      await ContactService.updateContact(contact, contact.id);
+      navigate(-1);
+    } catch (err) {}
+  };
   return (
     <>
-        {/* <p style={{color : "white"}}>Edit contact page...</p> */}
-        {/* <p style={{color : "white"}}> {store.getState().email } </p> */}
-        <div className="container-fluid pt-4" style={{ maxWidth : "80%", paddingRight : "5%"}}>
+      <div
+        className="container-fluid pt-4"
+        style={{ maxWidth: "80%", paddingRight: "5%" }}
+      >
         <h3 className="py-3 text-light">Edit Contact</h3>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group pb-4">
-          {/* <label className="text-white y-3 bg-secondary p-2 pb-3">Name</label> */}
-          <input
-          style={{color : "white"}}
-            type="text"
-            className="add-con-input bg-secondary my-3 form-control"
-            id="exampleInputEmail1"
-            placeholder="Enter Name"
-            defaultValue="Gokul"
+        <form onSubmit={handleSubmit}>
+          <div className="form-group pb-4">
+            <input
+              name="name"
+              style={{ color: "white" }}
+              type="text"
+              className="add-con-input bg-secondary my-3 form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Name"
+              defaultValue={contact.name}
+              onChange={handleChange}
             />
-        </div>
-        <div className="form-group pb-4">
-          {/* <label className="text-white y-3 bg-secondary p-2 pb-3">Mobile number</label> */}
-          <input
-          style={{color : "white"}}
-            type="number"
-            className="add-con-input bg-secondary my-3 form-control"
-            id="exampleInputPassword1"
-            placeholder="Mobile number"
-            defaultValue="987654321"
+          </div>
+          <div className="form-group pb-4">
+            <input
+              name="phoneno"
+              style={{ color: "white" }}
+              type="number"
+              className="add-con-input bg-secondary my-3 form-control"
+              id="exampleInputPassword1"
+              placeholder="Mobile number"
+              defaultValue={contact.phoneno}
+              onChange={handleChange}
             />
-        </div>
-        <div className="form-group pb-4">
-          {/* <label className="text-white y-3 bg-secondary p-2 pb-3">Email address</label> */}
-          <input
-          style={{color : "white"}}
-            type="email"
-            className="add-con-input bg-secondary my-3 form-control"
-            id="exampleInputEmail1"
-            placeholder="Enter email"
-            defaultValue="gokul@gmail.com"
+          </div>
+          <div className="form-group pb-4">
+            <input
+              name="email"
+              style={{ color: "white" }}
+              type="email"
+              className="add-con-input bg-secondary my-3 form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter email"
+              defaultValue={contact.email}
+              onChange={handleChange}
             />
-        </div>
-        <div className="form-group pb-4">
-          {/* <label className="text-white y-3 bg-secondary p-2 pb-3">Group</label> */}
-          <input
-          style={{color : "white"}}
-            type=""text
-            className="add-con-input bg-secondary my-3 form-control"
-            id="exampleInputEmail1"
-            placeholder="Enter group name"
-            defaultValue=""
+          </div>
+          <div className="form-group pb-4">
+            <input
+              name="groupname"
+              style={{ color: "white" }}
+              type="text"
+              className="add-con-input bg-secondary my-3 form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter group name"
+              defaultValue={contact.groupname}
+              onChange={handleChange}
             />
-        </div>
-        <div className="pt-4">
-        <button className="btn btn-primary d-flex align-items-center">
-          Submit
-        </button>
-        </div>
-      </form>
-            </div>
+          </div>
+          <div className="pt-4">
+            <button className="btn btn-primary d-flex align-items-center">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default EditContact
+export default EditContact;
