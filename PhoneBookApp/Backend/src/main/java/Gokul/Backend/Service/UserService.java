57fea8a5f70b547f284bea6e59d4594fdcd5ada7
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:26:06 +0530

    Update UserService.java

--- PhoneBookApp/Backend/src/main/java/Gokul/Backend/Service/UserService.java
+++ PhoneBookApp/Backend/src/main/java/Gokul/Backend/Service/UserService.java
@@ -1,10 +1,12 @@
 package Gokul.Backend.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import Gokul.Backend.DTO.UserDTO;
 import Gokul.Backend.Entity.User;
 import Gokul.Backend.Repository.UserRepository;
 
@@ -14,7 +16,39 @@ public class UserService {
 	@Autowired
 	UserRepository uRepo;
 	
-	public List<User> getAll() {
+	public List<UserDTO> getAll() {
+		
+		List<User> user = uRepo.findAll();
+		List<UserDTO> userDTO = new ArrayList<>();
+		
+		for(User u : user) {
+			
+			userDTO.add( UserDTO
+			.builder()
+			.name(u.getName())
+			.email(u.getEmail())
+			.dob(u.getDob())
+			.phoneno(u.getPhoneno())
+			.build());
+		}
+		
+		return userDTO;
+	}
+
+	public UserDTO getById(Long id) {
+		
+		User user = uRepo.findById(id).get();
+		
+		return UserDTO
+				.builder()
+				.name(user.getName())
+				.email(user.getEmail())
+				.dob(user.getDob())
+				.phoneno(user.getPhoneno())
+				.build();
+	}
+
+	public List<User> getAllUser() {
 		
 		return uRepo.findAll();
 	}
