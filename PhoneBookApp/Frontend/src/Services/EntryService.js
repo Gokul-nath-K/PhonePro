be4ba3bd2ee29e5fdf59b824d46966e2057f9552
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:27:25 +0530

    Create EntryService.js

---
+++ PhoneBookApp/Frontend/src/Services/EntryService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { BASE_URL } from "../Constants/ApiConstant"
+
+const createNewUser = async (request) => {
+
+    const response = await axios.post(`${BASE_URL}/entry/post`, request);
+    return response.data;
+};
+
+const validateUser = async (request) => {
+    const response = await axios.post(`${BASE_URL}/entry/auth_user`, request);
+    console.log(response.data);
+    return response.data;
+};
+
+export const EntryService = {
+    createNewUser,
+    validateUser
+}
