00b6720c002a019267e6109f3efb042bb427cea2
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:26:27 +0530

    Update Store.jsx

--- PhoneBookApp/Frontend/src/Assets/Redux/Store.jsx
+++ PhoneBookApp/Frontend/src/Assets/Redux/Store.jsx
@@ -1,7 +1,16 @@
-import reducer from "./Reducer";
-import { legacy_createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "../Slices/UserSlice";
+import ContactReducer from "../Slices/ContactSlice";
 
-
-const store = legacy_createStore(reducer);
+const store = configureStore({
+  reducer: {
+    user: UserReducer,
+    contact: ContactReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
 export default store;
