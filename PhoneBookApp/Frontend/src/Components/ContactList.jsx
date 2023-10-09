0f44cb6130c652698c01db0f773411dec41209b4
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:26:42 +0530

    Update ContactList.jsx

--- PhoneBookApp/Frontend/src/Components/ContactList.jsx
+++ PhoneBookApp/Frontend/src/Components/ContactList.jsx
@@ -1,36 +1,111 @@
-import React from "react";
+import { PencilSquare, Trash3Fill } from "react-bootstrap-icons";
 import { contacts } from "../Assets/Contexts/Contacts";
+import { useState } from "react";
+
 const ContactList = () => {
+  const array = Array.from({ length: 26 }, (value, index) => index + 1);
+  const [style, setStyle] = useState({ display: "none" });
+  const [isHovering, setIsHovering] = useState(-1);
+  const handleMouseEnter = (i) => {
+    setIsHovering(i);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(-1);
+  };
+
   return (
     <>
-      <div className="container-fluid" style={{ paddingRight: "5%" }}>
+      <div
+        className=" contact-list container-fluid"
+        style={{ paddingRight: "5%" }}
+      >
         <div className="row ">
           <h3 className="py-3 px-4 text-light">All contacts</h3>
         </div>
         <div className="row" style={{ height: "100vh" }}>
           <div className="col overflow-y-auto mx-3" style={{ height: "100%" }}>
-            {contacts &&
-              contacts.map((contact) => {
+            {array &&
+              array.map((num) => {
+                let firstletter = String.fromCharCode(64 + num).toLowerCase();
                 return (
-                  <div key={contact.id}>
-                    <div className="row bg-transparent border-bottom py-2">
-                      <div className="col-1">
-                        <button
-                          type="button"
-                          className="btn btn-secondary rounded-circle"
-                          disabled
-                        >
-                          {contact.name.slice(0, 1)}
-                        </button>
-                      </div>
+                  <div key={num}>
+                    <div
+                      className="row bg-transparent border-bottom py-1 mt-3 mb-3"
+                      id={firstletter}
+                    >
                       <div className="col">
-                        <p className="pt-2 text-light"> {contact.name} </p>
+                        <p className="pt-2 text-light fw-bold fs-3">
+                          {firstletter.toUpperCase()}
+                        </p>
                       </div>
                     </div>
+                    {contacts &&
+                      contacts
+                        .filter(
+                          (contact) =>
+                            contact.name.slice(0, 1).toLowerCase() ===
+                            firstletter
+                        )
+                        .map((contact) => {
+                          return (
+                            <div key={contact.id}>
+                              <div
+                                className="row bg-transparent pt-2 pb-2"
+                                onMouseEnter={() =>
+                                  handleMouseEnter(contact.id)
+                                }
+                                onMouseLeave={handleMouseLeave}
+                              >
+                                <div className="col-1 pt-1">
+                                  <button
+                                    type="button"
+                                    className="btn btn-secondary rounded fs-5"
+                                    disabled
+                                  >
+                                    {contact.name.slice(0, 1)}
+                                  </button>
+                                </div>
+                                <div className="col pt-1">
+                                  <p className="pt-2 text-light">
+                                    {contact.name}
+                                  </p>
+                                </div>
+                                <div className="col-1 pt-1">
+                                  <button
+                                    type="button"
+                                    className="btn btn-secondary rounded fs-5"
+                                    style={
+                                      isHovering == contact.id
+                                        ? { display: "block" }
+                                        : style
+                                    }
+                                  >
+                                    <PencilSquare />
+                                  </button>
+                                </div>
+                                <div className="col-1 pt-1">
+                                  <button
+                                    type="button"
+                                    className="btn btn-secondary rounded fs-5"
+                                    style={
+                                      isHovering == contact.id
+                                        ? { display: "block" }
+                                        : style
+                                    }
+                                  >
+                                    <Trash3Fill />
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        })}
                   </div>
                 );
               })}
           </div>
+
           <div
             className="col-1 overflow-y-auto d-flex justify-content-center bg-transparent"
             style={{ height: "100%" }}
@@ -39,84 +114,23 @@ const ContactList = () => {
               className="list-group bg-transparent"
               style={{ fontSize: "12px" }}
             >
-              <li className="list-group-item bg-transparent text-light border-0">
-                A
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                B
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                C
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                D
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                E
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                F
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                G
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                H
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                I
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                J
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                K
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                L
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                M
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                N
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                O
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                P
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                Q
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                R
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                S
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                T
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                U
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                V
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                W
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                X
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                Y
-              </li>
-              <li className="list-group-item bg-transparent text-light border-0">
-                Z
-              </li>
+              {array &&
+                array.map((num) => {
+                  let firstletter = String.fromCharCode(64 + num).toLowerCase();
+                  return (
+                    <a
+                      key={num}
+                      href={"#" + firstletter}
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      <button className="btn bg-transparent">
+                        <li className="list-group-item bg-transparent text-light border-0 fs-6">
+                          {String.fromCharCode(64 + num)}
+                        </li>
+                      </button>
+                    </a>
+                  );
+                })}
             </ul>
           </div>
         </div>
