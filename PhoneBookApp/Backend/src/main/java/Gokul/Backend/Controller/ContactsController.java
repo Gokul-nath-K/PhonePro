34302cc72b4b020028a2c8f07451b54321beafbb
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:24:56 +0530

    Update ContactsController.java

--- PhoneBookApp/Backend/src/main/java/Gokul/Backend/Controller/ContactsController.java
+++ PhoneBookApp/Backend/src/main/java/Gokul/Backend/Controller/ContactsController.java
@@ -5,25 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import Gokul.Backend.Constants.Api;
 import Gokul.Backend.Entity.Contacts;
 import Gokul.Backend.Service.ContactsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
-
+@CrossOrigin
 @RestController
-@RequestMapping("/contacts")
+@RequestMapping(Api.CONTACT)
 public class ContactsController {
 
 	@Autowired
