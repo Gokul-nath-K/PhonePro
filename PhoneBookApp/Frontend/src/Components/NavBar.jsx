528f8c5f996711392388edfa162cbe5514360279
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:26:55 +0530

    Update NavBar.jsx

--- PhoneBookApp/Frontend/src/Components/NavBar.jsx
+++ PhoneBookApp/Frontend/src/Components/NavBar.jsx
@@ -1,46 +1,50 @@
 import React from "react";
 import { TelephoneFill } from "react-bootstrap-icons";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { setActive } from "../Assets/Slices/UserSlice";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+
   return (
-    <>
-      <nav
-        id="navbar-id"
-        className="navbar navbar-expand-lg"
-        style={{ height: "9%", zIndex :"999", backgroundColor: "#272829" }}
-      >
-        <div className="container-fluid">
-          <Link style={{ color: "white" }} className="navbar-brand px-3" to="/">
-            <TelephoneFill size={20} className="mx-2"/>
-            <b>
-              PhonePro
-            </b>
-          </Link>
-          <div className="d-flex">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link
-                  style={{ color: "white" }}
-                  className="nav-link"
-                  to="/signin"
-                >
-                  Sign in
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  style={{ color: "white" }}
-                  className="nav-link"
-                  to="/signin"
-                >
-                  Sign up
-                </Link>
-              </li>
-            </ul>
+    <>     
+        <nav
+          id="navbar-id"
+          className="navbar navbar-expand-lg"
+          style={{ height: "9%", zIndex: "999", backgroundColor: "#272829" }}
+        >
+          <div className="container-fluid">
+            <Link style={{ color: "white" }} className="navbar-brand px-3" to="/">
+              <TelephoneFill size={20} className="mx-2" />
+              <b>PhonePro</b>
+            </Link>
+            <div className="d-flex">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link
+                    style={{ color: "white" }}
+                    className="nav-link"
+                    to="/signin"
+                    onClick={() => dispatch(setActive({ active: "signin" }))}
+                  >
+                    Sign in
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    style={{ color: "white" }}
+                    className="nav-link"
+                    to="/signin"
+                    onClick={() => dispatch(setActive({ active: "signup" }))}
+                  >
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
     </>
   );
 };
