bea06432d8df9d57740b7a05b0d48b1b0ca8bba6
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:27:16 +0530

    Update SignupForm.jsx

--- PhoneBookApp/Frontend/src/Pages/SignupForm.jsx
+++ PhoneBookApp/Frontend/src/Pages/SignupForm.jsx
@@ -1,29 +1,82 @@
 import React, { useContext } from "react";
-import { BoldLink, BoxContainer, FormContainer, Input, LineText, SubmitButton } from "../Components/Elements";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  LineText,
+  SubmitButton,
+} from "../Components/Elements";
 import { AccountContext } from "../Assets/Contexts/AccountContext";
 
 const SignupForm = (props) => {
-
-    const { switchToSignin, handleChange, handleSubmit, error } = useContext(AccountContext);
+  const { switchToSignin, handleChange, handleSubmit, classes } =
+    useContext(AccountContext);
 
   return (
     <>
       <BoxContainer>
         <FormContainer>
-          <Input onChange={ handleChange } name="fullname" type="text" placeholder="Full name" />
-          <Input onChange={ handleChange } name="email" type="email" placeholder="Email" />
-          <span style={{ color: 'red', fontSize: 'small', textAlign: 'start', display: 'flex', padding: 4, paddingLeft: 10 }} > {error.email} </span>
-          <Input onChange={ handleChange } name="password" type="password" placeholder="Password" />
-          <span style={{ color: 'red', fontSize: 'small', textAlign: 'start', display: 'flex', padding: 4, paddingLeft: 10 }} > {error.password } </span>
-          <Input onChange={ handleChange } name="cpassword" type="password" placeholder="Confirm password" />
+          <Input
+            onChange={handleChange}
+            name="name"
+            type="text"
+            placeholder="Full name"
+            className={"form-control " + classes.name}
+          />
+          <Input
+            onChange={handleChange}
+            name="phoneno"
+            type="number"
+            placeholder="Phone number"
+            className={"form-control " + classes.name}
+          />
+          <Input
+            onChange={handleChange}
+            name="dob"
+            type="text"
+            placeholder="Date of Birth"
+            className={"form-control " + classes.name}
+          />
+          <Input
+            onChange={handleChange}
+            name="email"
+            type="email"
+            placeholder="Email"
+            className={"form-control " + classes.email}
+          />
+          <Input
+            onChange={handleChange}
+            name="password"
+            type="password"
+            placeholder="Password"
+            className={"form-control " + classes.password}
+          />
+          <Input
+            onChange={handleChange}
+            name="cpassword"
+            type="password"
+            placeholder="Confirm password"
+            className={"form-control " + classes.password}
+          />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              width: "100%",
+              flexDirection: "column",
+              alignItems: "center",
+              marginTop: "2%",
+            }}
+          >
+            <SubmitButton onClick={handleSubmit} type="submit">
+              Signup
+            </SubmitButton>
+          </div>
         </FormContainer>
-        <span style={{ "padding" : "6px"}}>{""}</span>
-        <SubmitButton onClick={handleSubmit} type="submit">Signup</SubmitButton>
         <LineText>
           Already have an account?{" "}
-          <BoldLink onClick={switchToSignin} >
-            Signin
-          </BoldLink>
+          <BoldLink onClick={switchToSignin}>Signin</BoldLink>
         </LineText>
       </BoxContainer>
     </>
