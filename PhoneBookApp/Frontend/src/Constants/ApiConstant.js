1c63f64598bbea190ed9f8bdc1b649eda9ee92ba
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-09 18:27:01 +0530

    Create ApiConstant.js

---
+++ PhoneBookApp/Frontend/src/Constants/ApiConstant.js
@@ -0,0 +1,11 @@
+
+let jwtToken = localStorage.getItem(`Token`);
+let authHeader = `Bearer ${jwtToken}`;
+
+export const HEADERS = {
+    'Authorization': authHeader,
+    'Content-Type': 'application/json',
+    'serializableCheck': false
+}
+
+export const BASE_URL = 'http://localhost:8083/api/v1';
