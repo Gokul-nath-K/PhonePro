fd0ccadc97baf53117fbfc1cdb38cb94c96529a2
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-10 02:46:33 +0530

    Update UserService.java

--- PhoneBookApp/Backend/src/main/java/Gokul/Backend/Service/UserService.java
+++ PhoneBookApp/Backend/src/main/java/Gokul/Backend/Service/UserService.java
@@ -25,26 +25,30 @@ public class UserService {
 			
 			userDTO.add( UserDTO
 			.builder()
-			.name(u.getName())
-			.email(u.getEmail())
-			.dob(u.getDob())
-			.phoneno(u.getPhoneno())
-			.build());
+					.id(u.getId())
+					.name(u.getName())
+					.email(u.getEmail())
+					.dob(u.getDob())
+					.phoneno(u.getPhoneno())
+					.role(u.getRole().toString())
+					.build());
 		}
 		
 		return userDTO;
 	}
 
-	public UserDTO getById(Long id) {
+	public UserDTO getByEmail(String email) {
 		
-		User user = uRepo.findById(id).get();
+		User user = uRepo.findByEmail(email).get();
 		
 		return UserDTO
 				.builder()
+				.id(user.getId())
 				.name(user.getName())
 				.email(user.getEmail())
 				.dob(user.getDob())
 				.phoneno(user.getPhoneno())
+				.role(user.getRole().toString())
 				.build();
 	}
 
@@ -52,5 +56,19 @@ public class UserService {
 		
 		return uRepo.findAll();
 	}
-	
+
+	public UserDTO getById(Long id) {
+
+		User user =  uRepo.findById(id).get();
+
+		return UserDTO
+				.builder()
+				.id(user.getId())
+				.name(user.getName())
+				.email(user.getEmail())
+				.dob(user.getDob())
+				.phoneno(user.getPhoneno())
+				.role(user.getRole().toString())
+				.build();
+	}
 }
