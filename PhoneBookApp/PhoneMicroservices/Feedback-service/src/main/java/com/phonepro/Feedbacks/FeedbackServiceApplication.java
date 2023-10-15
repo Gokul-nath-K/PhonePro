faccdc78cd45e7509c8b0af19d21772019963cfd
Gokul-nath-K <gokulnathk003@gmail.com>
2023-10-15 13:09:17 +0530

    microservice added

---
+++ PhoneBookApp/PhoneMicroservices/Feedback-service/src/main/java/com/phonepro/Feedbacks/FeedbackServiceApplication.java
@@ -0,0 +1,15 @@
+package com.phonepro.Feedbacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FeedbackServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FeedbackServiceApplication.class, args);
+    }
+}
